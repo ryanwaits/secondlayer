092503bec9ee6661dbee83b538528bf9b6c1a6b2
Ryan Waits <ryan.waits@gmail.com>
2026-03-09 14:31:10 -0500

    feat(sdk): add Contracts client for search, get, getAbi

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -3,15 +3,18 @@ import { BaseClient } from "./base.ts";
 import type { SecondLayerOptions } from "./base.ts";
 import { Streams } from "./streams/client.ts";
 import { Views } from "./views/client.ts";
+import { Contracts } from "./contracts/client.ts";
 
 export class SecondLayer extends BaseClient {
   readonly streams: Streams;
   readonly views: Views;
+  readonly contracts: Contracts;
 
   constructor(options: Partial<SecondLayerOptions> = {}) {
     super(options);
     this.streams = new Streams(options);
     this.views = new Views(options);
+    this.contracts = new Contracts(options);
   }
 
   async getQueueStats(): Promise<QueueStats> {
