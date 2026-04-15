b1c0657515f17d7b2035d7a9ec78f021fd07c52a
Ryan Waits <ryan.waits@gmail.com>
2026-04-15 14:45:46 -0500

    ops: cut postgres backup retention from 7 to 3 days

--- docker/scripts/backup-postgres.sh
+++ docker/scripts/backup-postgres.sh
@@ -9,7 +9,7 @@ BACKUP_DIR="${DATA_DIR}/backups/postgres"
 CONTAINER="${PG_CONTAINER:-secondlayer-postgres-1}"
 PG_USER="${PG_USER:-secondlayer}"
 PG_DB="${PG_DB:-secondlayer}"
-RETENTION_DAYS=7
+RETENTION_DAYS=3
 DATE=$(date +%Y%m%d-%H%M%S)
 
 log() { echo "[$(date -Iseconds)] $*"; }
