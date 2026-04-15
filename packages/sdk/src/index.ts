a056f6b77e98ba021c770e676bde95d61dc40cba
Ryan Waits <ryan.waits@gmail.com>
2026-04-14 20:54:39 -0500

    feat!: remove streams from SDK - BREAKING CHANGE for v2.0

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,7 +1,6 @@
 export { SecondLayer } from "./client.ts";
 export type { SecondLayerOptions } from "./base.ts";
 export { Marketplace } from "./marketplace/index.ts";
-export { Streams } from "./streams/index.ts";
 export { Subgraphs, getSubgraph } from "./subgraphs/index.ts";
 export { Workflows } from "./workflows/index.ts";
 export { ApiError, VersionConflictError } from "./errors.ts";
