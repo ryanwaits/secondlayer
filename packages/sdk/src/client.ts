d97fa4c3d299f321a77aa721f37a5e401173a527
Ryan Waits <ryan.waits@gmail.com>
2026-04-23 11:36:47 -0500

    feat(sdk)!: remove Sentries client; subgraphs only

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -1,15 +1,12 @@
 import { BaseClient } from "./base.ts";
 import type { SecondLayerOptions } from "./base.ts";
-import { Sentries } from "./sentries/client.ts";
 import { Subgraphs } from "./subgraphs/client.ts";
 
 export class SecondLayer extends BaseClient {
 	readonly subgraphs: Subgraphs;
-	readonly sentries: Sentries;
 
 	constructor(options: Partial<SecondLayerOptions> = {}) {
 		super(options);
 		this.subgraphs = new Subgraphs(options);
-		this.sentries = new Sentries(options);
 	}
 }
--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,12 +1,5 @@
 export { SecondLayer } from "./client.ts";
 export type { SecondLayerOptions } from "./base.ts";
 export { Subgraphs, getSubgraph } from "./subgraphs/index.ts";
-export { Sentries } from "./sentries/client.ts";
-export type {
-	SentrySummary,
-	SentryDetail,
-	SentryAlert,
-	SentryKindInfo,
-} from "./sentries/client.ts";
 export { ApiError, VersionConflictError } from "./errors.ts";
 export { verifyWebhookSignature } from "./webhooks.ts";
